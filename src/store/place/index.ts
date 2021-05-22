109ca62504517c18ca5a8255d14c7e2dcc4fb6c7
haewonc <haewon858@gmail.com>
2021-05-23 00:51:17 +0900

    f

--- src/store/place/index.ts
+++ src/store/place/index.ts
@@ -1,105 +1,50 @@
 import { PayloadAction } from '@reduxjs/toolkit';
 import { buffers } from 'redux-saga';
+import { isTemplateExpression } from 'typescript';
 import { createSlice } from 'utils/@reduxjs/toolkit'; // Importing from `utils` makes them more type-safe ✅
 
 import { useInjectReducer } from 'utils/redux-injectors';
-
+import places from "./restaurants.json";
 import { IPlace, PlacesState } from './types';
 
-// The initial state of the Homepage
-function choice<T>(arr:Array<T>){
-    return arr[Math.min(arr.length-1, Math.round(Math.random()*arr.length))]
-}
-
-function random_human(){
-    return choice([ 'Sophia Kang', 'David Kim', 'Jonghyeon King', 'Heejun Lee', 'Minji Lee', 
-    'Minuk Kim', 'Iviy Roson', 'Krukgej Frank', 'Matthew Miller', 'Ahmed Ashmar', 'Abdul Rebul', 'Tanaka Nakayoshi'])
-}
-
-function random_name() {
-    return choice([
-        'BHC', "Wolfgang's Steakhouse", "Donkey Chicken", "BBQ", "Outback", "GS25", "Seven Eleven", 
-        "Hanaro Mart", "Hi Mart", "Samyukga", "Lotteria", "Burgerking", "Mcdonalds", "KFC", "Outdack", "Yellow Chicken", "Moms Touch"
-    ])
-}
-
-function random_picture() {
-    let buf = new Array()
-    for(let i =0; i<5; i++){
-        buf.push(choice([
-            'https://media.timeout.com/images/103504187/630/472/image.jpg',
-            'https://resizer.otstatic.com/v2/photos/wide-huge/1/30102456.jpg',
-            'https://mp-seoul-image-production-s3.mangoplate.com/192731/p1erzbe2v6e9ig.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80',
-            'https://qul.imgix.net/030160f4-7998-4d7f-88e3-ba0e92929220/558553_sld.jpg',
-            'https://dimg.donga.com/a/500/0/90/5/ugc/CDB/29STREET/Article/5e/b2/04/e8/5eb204e81752d2738236.jpg',
-            'https://dimg.donga.com/wps/NEWS/IMAGE/2021/01/11/104866432.2.jpg',
-            'http://res.heraldm.com/phpwas/restmb_idxmake.php?idx=507&simg=/content/image/2019/09/27/20190927000594_0.jpg',
-            'https://img.khan.co.kr/news/2019/05/30/l_2019053101003584700279572.jpg',
-            'https://s3.ap-northeast-2.amazonaws.com/img.kormedi.com/news/article/__icsFiles/artimage/2015/05/23/c_km601/432212_540.jpg',
-            'https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/8e0d02fc-c587-415b-aa23-4b218b2fd3c2.jpeg',
-            'https://s3.ap-northeast-2.amazonaws.com/img.kormedi.com/news/article/__icsFiles/artimage/2016/03/29/c_km601/911811_540.jpg',
-            'http://t1.daumcdn.net/liveboard/realfood/ae1ba57635dc409e9bf6d970f4578780.JPG',
-            'https://t1.daumcdn.net/liveboard/interstella-story/af605d13c3894dc8bc0a20407baf54e9.JPG',
-            'https://www.sisaweekly.com/news/photo/202005/31622_47966_2831.jpg',
-        ]))
-    }
-    return buf
-}
-
-function random_location() {
-    return {
-        longitude: Math.random()*0.1-74.0,
-        latitude: Math.random()*0.08+40.7,
-        readable: '4 Park Ave, New York'
-    }
-}
-
 let generated_places = new Array()
-for(let i=0; i<10; i++)
+for(let i=0; i<4; i++)
 {
     generated_places.push(
         {
             id: i+1,
-            name: random_name(),
-            address: random_location(),
+            name: (<any>places).places.restaurants[i].name,
+            address: {
+                longitude: (<any>places).places.restaurants[i].address.longitude,
+                latitude: (<any>places).places.restaurants[i].address.latitude,
+                readable: (<any>places).places.address
+            },
             distance: Math.round(Math.random()*500+500), // meter
             travel_time: Math.round(Math.random()*60+5), // minute
-            waiting_time: Math.round(Math.random()*300+60), // minute
+            waiting_time: (<any>places).places.restaurants[i].waiting_time, // minute
             open_time: '09:00',
             close_time: '22:00',
             local_time: `${Math.round(Math.random()*23)+1}:00`,
-            tags: ['steak', 'luxery'],
-            rating: Math.random()*5,
+            tags: (<any>places).places.restaurants[i].tags,
+            rating: (<any>places).places.restaurants[i].rating,
             ai_pick: Math.random() > 0.5,
-            ai_score: Math.random()*5,
-            picture_urls: random_picture(),
+            ai_score: (<any>places).places.restaurants[i].ai_score,
+            picture_urls: (<any>places).places.restaurants[i].picture_urls,
             reviews: [
                 {
-                    author: random_human(),
-                    content: 'The Wolfgang Steak is one of the three biggest steaks in New York! It felt really different from Peter Ruger. Peter Ruger was the soft feeling of a carefully raised cow, while the Wolfgang was the feeling of a wildly raised cow. The flesh was a bit tough and strong.',
-                    rating: Math.random()*5,
+                    author: (<any>places).places.restaurants[i].reviews[0].author,
+                    content: (<any>places).places.restaurants[i].reviews[0].content,
+                    rating: (<any>places).places.restaurants[i].reviews[0].rating,
                     attachment_urls: []
                 },
                 {
-                    author: random_human(),
-                    content: 'The Wolfgang Steak is one of the three biggest steaks in New York! It felt really different from Peter Ruger. Peter Ruger was the soft feeling of a carefully raised cow, while the Wolfgang was the feeling.',
-                    rating: Math.random()*5,
-                    attachment_urls: random_picture(),
-                },
-                {
-                    author: random_human(),
-                    content: 'The Wolfgang Steak is one of the three biggest steaks in New York! It felt really different from Peter Ruger. Peter Ruger was the soft feeling of a carefully raised cow, while the Wolfgang was the feeling of a wildly raised cow. The flesh was a bit tough and strong.',
-                    rating: Math.random()*5,
-                    attachment_urls: []
-                },
-                {
-                    author: random_human(),
-                    content: 'The Wolfgang Steak is one of the three biggest steaks in New York! It felt really different from Peter Ruger. Peter Ruger was the soft feeling of a carefully raised cow, while the Wolfgang was the feeling of a wildly raised cow. The flesh was a bit tough and strong.',
-                    rating: Math.random()*5,
+                    author: (<any>places).places.restaurants[i].reviews[1].author,
+                    content: (<any>places).places.restaurants[i].reviews[1].content,
+                    rating: (<any>places).places.restaurants[i].reviews[1].rating,
                     attachment_urls: []
                 }
             ],
-            menus: [],
+            menus: (<any>places).places.restaurants[i].reviews[1].menus,
             submenu_opened: false,
             submenu_selected: false,
         }
@@ -187,6 +132,38 @@ const slice = createSlice({
             }
             return state;
         },
+
+        updateMenu(state: PlacesState, action: PayloadAction<UpdateMenuPayloadType>)
+        {
+            const payload = action.payload as UpdateMenuPayloadType;
+            for(let i=0; i<state.places.length; i++)
+            {
+                let item = state.places[i];
+                if(item.id == payload.id){
+                    item.menus = payload.data;
+                    break;
+                }
+            }
+            return state;
+        },
+
+        openMenu(state: PlacesState)
+        {
+            state.menu_viewer_opened = true;
+            return state;
+        },
+
+        closeMenu(state: PlacesState)
+        {
+            state.menu_viewer_opened = false;
+            return state;
+        },
+
+        toggleMenu(state: PlacesState)
+        {
+            state.menu_viewer_opened = !state.menu_viewer_opened;
+            return state;
+        }
     },
 });
 
