04fde408b7b9b40550f6dcb4dc29bdacb474e1a1
AinL <gmlwns5176@gmail.com>
2021-05-23 17:20:24 +0900

    add: link modal

--- src/app/components/LeftMenu/RestaurantPopupList/RestaurantPopup/RestaurantPartyList/RestaurantPartyListItem.tsx
+++ src/app/components/LeftMenu/RestaurantPopupList/RestaurantPopup/RestaurantPartyList/RestaurantPartyListItem.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { IParty } from 'store/party/types';
 import styled from 'styled-components/macro';
 
+import PartyRegisteraion from 'app/components/Modals/PartyRegisteraion';
 import PeopleIcon from './PeopleIcon';
 
 const Box = styled.div`
@@ -56,6 +57,8 @@ interface PropsType {
 export default function RestaurantPartyListItem(props : PropsType) {
     const { party } = props;
 
+    const [isPartyRegisteraionOn, setIsPartyRegisteraionOn] = React.useState(false);
+
     let range = new Array<number>();
     for(let i = 0; i < party.max_people; i++) range.push(i);
 
@@ -74,11 +77,16 @@ export default function RestaurantPartyListItem(props : PropsType) {
                     }
                 </PeopleBox>
             </ContentBox>
-            <DetailButton>
+            <DetailButton onClick={()=>setIsPartyRegisteraionOn(true)}>
                 <svg width="16" height="4" viewBox="0 0 16 4" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M2 0C0.9 0 0 0.9 0 2C0 3.1 0.9 4 2 4C3.1 4 4 3.1 4 2C4 0.9 3.1 0 2 0ZM14 0C12.9 0 12 0.9 12 2C12 3.1 12.9 4 14 4C15.1 4 16 3.1 16 2C16 0.9 15.1 0 14 0ZM8 0C6.9 0 6 0.9 6 2C6 3.1 6.9 4 8 4C9.1 4 10 3.1 10 2C10 0.9 9.1 0 8 0Z" fill="#FAFAFA"/>
                 </svg>
             </DetailButton>
+            <PartyRegisteraion
+                selectedId={party?.id}
+                isPartyRegisteraionOn={isPartyRegisteraionOn}
+                setIsPartyRegisteraionOn={setIsPartyRegisteraionOn}
+            />
         </Box>
     )
 }
